7aa5969657f4a1ade595ba2de37b3210116a4d0a
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2025-11-24 14:24:07 +0100

    fix bugs after mentor review. Add useMutation (deletion via useMutation(deleteNote))

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,12 +1,22 @@
 import css from "./NoteList.module.css";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteNote } from "../../services/noteService";
 import type { Note } from "../../types/note";
 
 export interface NoteListProps {
   notes: Note[];
-  onDelete: (id: Note["id"]) => void;
 }
 
-export default function NoteList({ notes, onDelete }: NoteListProps) {
+export default function NoteList({ notes }: NoteListProps) {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (id: Note["id"]) => deleteNote(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+  });
+
   if (!notes.length) return null;
 
   return (
@@ -17,8 +27,12 @@ export default function NoteList({ notes, onDelete }: NoteListProps) {
           <p className={css.content}>{note.content}</p>
           <div className={css.footer}>
             <span className={css.tag}>{note.tag}</span>
-            <button className={css.button} onClick={() => onDelete(note.id)}>
-              Delete
+            <button
+              className={css.button}
+              onClick={() => mutation.mutate(note.id)}
+              disabled={mutation.isPending}
+            >
+              {mutation.isPending ? "Deleting..." : "Delete"}
             </button>
           </div>
         </li>
